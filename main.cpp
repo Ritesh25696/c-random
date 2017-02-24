d4888601d3e309dece6bd3f3d249ce897ebb44a4
ritesh chauhan <ritesh chauhan>
2017-02-24 14:12:10 +0530

    initial

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class human{
+private:
+    int *age;
+    string *name;
+
+public:
+    human(string iname,int a)
+    {age=new int;
+    name = new string;
+    *age=a;
+    *name=iname;
+    }
+
+    void display(){
+    cout<<*age<<endl<<*name<<endl;
+    }
+
+    ~human(){
+    cout<<"destructor at work";
+    delete name;
+    delete age;
+    }
+
+    };
+
+int main(){
+    human *aruna;
+    aruna=new human("aruna",100);
+    aruna->display();
+    delete aruna;
+return 0;}
